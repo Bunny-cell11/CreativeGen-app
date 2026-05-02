125d04a5e6ca4c610b136318b2506635526cc943
PAGIDI KONDALA BHAVANI <bhavanipagidikondala@gmail.com>
2026-05-02 18:15:13 +0530

    Add files via upload

---
+++ models/Creative.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const CreativeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  content: {
+    type: String,
+    default: ''
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Creative', CreativeSchema);
